a36bd708bbe362d2f38968a11e055fa77c9049f8
safi islam <118913244+safiislam@users.noreply.github.com>
2025-07-26 18:36:15 +0600

    add

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 import express from "express";
 import cors from "cors";
+import { multitenancy } from "express-multitenancy";
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+// app.use(
+//   multitenancy({
+//     getTenantId: (req) => {
+//       //   console.log(req);
+//       // Example: get tenant from header 'x-tenant-id'
+//       return req.headers["x-tenant-id"];
+//     },
+//   })
+// );
+app.get("/", async (req, res) => {
+  res.json({ message: "Hello" });
+});
 
 export default app;
